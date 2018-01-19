39226ceb64b764febddec26166d8e48adeaae0a8
Nathan Grey <nathan.grey@medsavvy.com>
2018-01-19 20:33:34 +0400

    still working

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,12 @@
 // ./react-redux-client/src/reducers/index.js
 import { routerReducer as routing } from 'react-router-redux';
 import { combineReducers } from 'redux';
-import appReducer from './appReducer';
-import {todoReducer} from './todoReducer';
+// import appReducer from './appReducer';
+// import {todoReducer} from './todoReducer';
 import {noteReducer} from './noteReducer';
 
 export default combineReducers({
-  appState:appReducer,
-  todoState:todoReducer,
+
   noteState:noteReducer,
   routing
   // More reducers if there are
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,9 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
-import Todos from './containers/Todos';
+// import Todos from './containers/Todos';
 import Notes from './containers/Notes';
-import Todo from './containers/Todo';
+// import Todo from './containers/Todo';
 
 export default (
   <Route path="/" component={App}>
